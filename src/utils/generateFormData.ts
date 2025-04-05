e4d10f7db79062dad245c35c0ceb95bd98f6f92d
chysis <chysiss@naver.com>
2025-04-06 00:12:42 +0900

    feat: axios 및 formdata 생성 유틸 함수 작성

---
+++ src/utils/generateFormData.ts
@@ -0,0 +1,22 @@
+const generateFormData = <T extends object>(data: T): FormData => {
+  const formData = new FormData();
+  const keys = Object.keys(data) as Array<keyof T>;
+
+  for (const key of keys) {
+    const value = data[key];
+
+    if (value !== undefined && value !== null) {
+      if (typeof value === 'boolean') {
+        formData.append(String(key), String(value));
+      } else if (value instanceof File || value instanceof Blob || typeof value === 'string') {
+        formData.append(String(key), value);
+      } else {
+        formData.append(String(key), String(value));
+      }
+    }
+  }
+
+  return formData;
+};
+
+export default generateFormData;
