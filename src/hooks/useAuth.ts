06621a50961894748bdf9ae70fe99f75e49d7cfd
chysis <chysiss@naver.com>
2025-04-10 19:21:36 +0900

    feat: 인증 관련 정보를 관리하는 useAuthStore zustand 훅 작성

--- src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-const useAuth = () => {
-  // 로그인 상태 관리 훅
-  // localStorage에 access_token이 있는지 확인하여 로그인 상태를 판단 (임시)
-  return {
-    isLoggedIn: !!localStorage.getItem('access_token'),
-  };
-};
-
-export default useAuth;
