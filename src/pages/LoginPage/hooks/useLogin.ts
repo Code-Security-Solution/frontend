63cb0d5969dc1039f314e0ae770fc68fe839a9c4
chysis <chysiss@naver.com>
2025-04-10 22:04:11 +0900

    feat: 로그인 폼 로직 훅으로 분리 및 유효성 검사 추가

---
+++ src/pages/LoginPage/hooks/useLogin.ts
@@ -0,0 +1,63 @@
+import { useAuthStore } from '@/stores/useAuthStore';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const useLogin = () => {
+  const navigate = useNavigate();
+  const { setAuthState } = useAuthStore();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleChangeEmail = (value: string) => {
+    setEmail(value);
+
+    if (value.trim().length > 0) setEmailError('');
+    else setEmailError('이메일을 입력해주세요.');
+  };
+
+  const handleChangePassword = (value: string) => {
+    setPassword(value);
+
+    if (value.trim().length > 0) setPasswordError('');
+    else setPasswordError('비밀번호를 입력해주세요.');
+  };
+
+  const handleClickToggleRegister = () => {
+    navigate('/register');
+  };
+
+  const isFormValid = email.trim().length > 0 && password.trim().length > 0;
+
+  const validateForm = () => {
+    if (!email.trim()) setEmailError('이메일을 입력해주세요.');
+    if (!password.trim()) setPasswordError('비밀번호를 입력해주세요.');
+  };
+
+  const handleClickLogin = () => {
+    if (!isFormValid) return validateForm();
+
+    // 테스트용 admin 로그인
+    if (email === 'admin' && password === '1234') {
+      alert('admin 로그인 성공');
+      setAuthState({ token: 'admin_token', userInfo: { email: 'admin@email.com' } });
+      navigate('/');
+    }
+  };
+
+  return {
+    email,
+    password,
+    emailError,
+    passwordError,
+    handleChangeEmail,
+    handleChangePassword,
+    handleClickToggleRegister,
+    handleClickLogin,
+  };
+};
+
+export default useLogin;
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,37 +1,21 @@
 import Input from '@/components/common/Input';
 import * as S from './styles';
-import { KeyboardEvent, useState } from 'react';
+import { KeyboardEvent } from 'react';
 import Button from '@/components/common/Button';
-import { useNavigate } from 'react-router-dom';
 import { formDropVarients } from '@/styles/motion';
-import { useAuthStore } from '@/stores/useAuthStore';
+import useLogin from './hooks/useLogin';
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-  const { setAuthState } = useAuthStore();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleChangeEmail = (value: string) => {
-    setEmail(value);
-  };
-
-  const handleChangePassword = (value: string) => {
-    setPassword(value);
-  };
-
-  const handleClickToggleRegister = () => {
-    navigate('/register');
-  };
-
-  const handleClickLogin = () => {
-    // 테스트용 admin 로그인
-    if (email === 'admin' && password === '1234') {
-      alert('admin 로그인 성공');
-      setAuthState({ token: 'admin_token', userInfo: { email: 'admin@email.com' } });
-      navigate('/');
-    }
-  };
+  const {
+    email,
+    password,
+    emailError,
+    passwordError,
+    handleChangeEmail,
+    handleChangePassword,
+    handleClickLogin,
+    handleClickToggleRegister,
+  } = useLogin();
 
   const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
     if (e.key === 'Enter') handleClickLogin();
@@ -44,8 +28,23 @@ const LoginPage = () => {
           <S.LoginFormTitle>로그인</S.LoginFormTitle>
           <S.LoginFormDescription>로그인 하면 이전에 분석한 기록을 볼 수 있어요.</S.LoginFormDescription>
         </S.LoginFormHeader>
-        <Input id="email" type="email" label="이메일" value={email} handleChange={handleChangeEmail} autoFocus />
-        <Input id="password" type="password" label="비밀번호" value={password} handleChange={handleChangePassword} />
+        <Input
+          id="email"
+          type="email"
+          label="이메일"
+          value={email}
+          errorMessage={emailError}
+          handleChange={handleChangeEmail}
+          autoFocus
+        />
+        <Input
+          id="password"
+          type="password"
+          label="비밀번호"
+          value={password}
+          errorMessage={passwordError}
+          handleChange={handleChangePassword}
+        />
         <Button styleType="secondary" onClick={handleClickLogin}>
           로그인
         </Button>
