672b07d5268066a7d66d3e4204ada11e13ae7344
chysis <chysiss@naver.com>
2025-05-02 03:07:56 +0900

    refactor: 헤더에서 access token이 있는 경우 사용자 정보를 불러오도록 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -4,10 +4,31 @@ import LogoIcon from '@/assets/Logo.svg';
 import ProfileInfo from '@/components/Profile/ProfileInfo';
 import UndraggableWrapper from '../UndraggableWrapper';
 import { useUserInfoStore } from '@/stores/useUserInfoStore';
+import { useAuthTokenStore } from '@/stores/useAuthTokenStore';
+import { getUserInfo } from '@/api/users';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 const Header = () => {
   const navigate = useNavigate();
-  const { isAuthenticated, userInfo } = useUserInfoStore();
+  const { accessToken } = useAuthTokenStore();
+  const { isAuthenticated, userInfo, setUserInfo } = useUserInfoStore();
+
+  const { data } = useQuery({
+    queryKey: ['userProfile', accessToken],
+    queryFn: getUserInfo,
+    enabled: !!accessToken,
+    staleTime: 60 * 60 * 1000,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setUserInfo({
+        email: data.email,
+        username: data.username,
+      });
+    }
+  }, [data]);
 
   const handleClickLogoIcon = () => {
     navigate('/');
