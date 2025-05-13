facd4f5fc179bb4cfe339adbdbc49291f88e678d
chysis <chysiss@naver.com>
2025-05-14 02:10:22 +0900

    fix: CORS 오류 해결

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -7,7 +7,6 @@ export const tokenAxios = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Credentials': 'true',
   },
 });
 
