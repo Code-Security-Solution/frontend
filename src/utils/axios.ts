4e2745943419f53dc18073eb519d64c4200a053b
chysis <chysiss@naver.com>
2025-05-14 17:39:09 +0900

    fix: 비회원도 분석 중 timeout 발생하는 문제 수정

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -22,7 +22,7 @@ tokenAxios.interceptors.request.use((config) => {
 
 export const publicAxios = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 15000,
+  timeout: 60 * 60 * 1000,
   headers: {
     'Content-Type': 'application/json',
   },
